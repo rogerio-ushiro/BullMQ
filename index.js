ea05d2fe5e3ec19cb173cfe87251b6a25ef6d9b7
rogerio-ushiro <rogerio.ushiro@gmail.com>
2026-04-10 16:01:50 +0200

    feat: keep listening new events

--- index.js
+++ index.js
@@ -10,10 +10,11 @@ if (!process.argv[2]) {
 ║   Usage: node index.js <option>                      ║
 ║   Example: node index.js 1                           ║
 ║                                                      ║
-║     1 — add jobs to the queue                        ║
-║     2 — process jobs from the queue                  ║
-║     3 — get queue statistics                         ║
-║     4 — retry failed jobs                            ║
+║    1 — add jobs to the queue                         ║
+║    2 — run and keep processing jobs from the queue   ║
+║    3 — get queue statistics                          ║
+║    4 — retry failed jobs                             ║
+║    5 — keep listening for jobs                       ║
 ║                                                      ║
 ╚══════════════════════════════════════════════════════╝
 `);
@@ -30,6 +31,9 @@ if (!process.argv[2]) {
             break;
         case '4':
             await import('./src/retry.js');
+            break;
+        case '5 ':
+            await import('./src/listener.js');
             break;
         default:
             console.log('Unknown argument. Please, try again.');
